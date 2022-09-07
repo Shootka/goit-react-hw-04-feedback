6e1100c5bbb02f5ea44410dd4bb03baa80d2077d
shootka <pirathaker315@gmail.com>
2022-09-07 14:16:11 +0300

    add note

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Notification from '../Notification/Notification';
 
 class Statistics extends Component {
   render() {
-    return (
-      <div>
-        <p style={{margin: '5px 0'}}>Good: {this.props.good}</p>
-        <p style={{margin: '5px 0'}}>Neutral: {this.props.neutral}</p>
-        <p style={{margin: '5px 0'}}>Bad: {this.props.bad}</p>
-        <p style={{margin: '5px 0'}}>Total: {this.props.total()}</p>
-        <p style={{margin: '5px 0'}}>Positive feedback: {this.props.positivePercentage()}%</p>
-      </div>
-    );
+
+    const { good, neutral, bad, total, positivePercentage } = this.props;
+    if (good || bad || neutral) {
+      return (
+        <div>
+          <p style={{ margin: '5px 0' }}>Good: {good}</p>
+          <p style={{ margin: '5px 0' }}>Neutral: {neutral}</p>
+          <p style={{ margin: '5px 0' }}>Bad: {bad}</p>
+          <p style={{ margin: '5px 0' }}>Total: {total()}</p>
+          <p style={{ margin: '5px 0' }}>Positive feedback: {positivePercentage()}%</p>
+        </div>
+      );
+    } else return <Notification/>
   }
 }
 
@@ -20,7 +25,7 @@ Statistics.propTypes = {
   neutral: PropTypes.number,
   bad: PropTypes.number,
   total: PropTypes.func,
-  positivePercentage: PropTypes.func
+  positivePercentage: PropTypes.func,
 };
 
 export default Statistics;
